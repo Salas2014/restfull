b2335d6d425ec49029fa9b845ce97c3aa6cba4a4
salas <salasengland33@gmail.com>
2021-12-11 02:41:33 +0200

    produces hypermedia-powered content!

--- src/main/java/com/salas/restfull/restfull/controller/EmployeeController.java
+++ src/main/java/com/salas/restfull/restfull/controller/EmployeeController.java
@@ -2,32 +2,53 @@ package com.salas.restfull.restfull.controller;
 
 import com.salas.restfull.restfull.entity.Employee;
 import com.salas.restfull.restfull.handlerException.EmployeeNotFoundException;
+import com.salas.restfull.restfull.modelAssembler.EmployeeModelAssembler;
 import com.salas.restfull.restfull.repo.EmployeeRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler employeeModelAssembler;
 
-    public EmployeeController(EmployeeRepository repository) {
+    public EmployeeController(EmployeeRepository repository, EmployeeModelAssembler employeeModelAssembler) {
         this.repository = repository;
+        this.employeeModelAssembler = employeeModelAssembler;
     }
 
     @GetMapping("/employees")
-    List<Employee> all(){
-       return repository.findAll();
+    public CollectionModel<EntityModel<Employee>> all(){
+
+        List<EntityModel<Employee>> employees = repository.findAll()
+                .stream()
+                .map(employeeModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+                linkTo(methodOn(EmployeeController.class)
+                        .all()).withSelfRel());
     }
+
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee employee){
         return repository.save(employee);
     }
+
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        return repository.findById(id)
+    public EntityModel<Employee> one(@PathVariable Long id){
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return employeeModelAssembler.toModel(employee);
     }
+
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
         return repository.findById(id)
---
+++ src/main/java/com/salas/restfull/restfull/modelAssembler/EmployeeModelAssembler.java
@@ -0,0 +1,22 @@
+package com.salas.restfull.restfull.modelAssembler;
+
+import com.salas.restfull.restfull.controller.EmployeeController;
+import com.salas.restfull.restfull.entity.Employee;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+    @Override
+    public EntityModel<Employee> toModel(Employee entity) {
+
+        return EntityModel.of(entity,
+                linkTo(methodOn(EmployeeController.class).one(entity.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")
+                );
+    }
+}
